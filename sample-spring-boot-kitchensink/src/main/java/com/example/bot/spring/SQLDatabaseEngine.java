6d5dedd849b222aa78e9654b57b9cfc08ad749f8
Patrick J. Wu <wotingwu@live.com>
2017-09-26 11:11:24 +0800

    Lab3

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -5,13 +5,36 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 @Slf4j
 public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
-		//Write your code here
+		String result = null;
+		Connection connection = this.getConnection();
+		PreparedStatement stmt = connection.prepareStatement("SELECT * FROM chatbot");
+		ResultSet rs = stmt.executeQuery();
+		
+		try {
+			
+			String sCurrentLine;
+			while (rs.next()) {
+				sCurrentLine = rs.getString(1) + ":" +  rs.getString(2);
+				String[] parts = sCurrentLine.split(":");
+				
+				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
+					result = parts[1];
+				}
+			}
+		}catch (Exception e) {
+			log.info("Exception while reading database: {}", e.toString());
+		}
+		if (result != null)
+			return result;
 		return null;
 	}
 	
